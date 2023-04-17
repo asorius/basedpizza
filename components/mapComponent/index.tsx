05def9eaf8cc53f8f839e4f4955ece3879fb9a04
asorius <18assdd@gmail.com>
2023-04-17 18:56:23 +0100

    infinite loop to locations on map

--- components/mapComponent/index.tsx
+++ components/mapComponent/index.tsx
@@ -10,8 +10,10 @@ import {
   Marker,
 } from 'react-simple-maps';
 import countries from './countries.json';
-const geoUrl = 'https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json';
-
+import worldCountriesJSON from 'assets/countries-110m.json';
+import Loading from 'lib/Loading';
+import Card from '@mui/material/Card';
+import Backdrop from '@mui/material/Backdrop';
 interface Props {
   countryList: string[];
 }
@@ -21,82 +23,117 @@ interface Marker {
   name: string;
   coordinates: [number, number];
 }
+const generateMarks = (countriesProp: string[]) => {
+  let markerList: Marker[] = [];
+  countries.forEach((co) => {
+    const name = co[3] as string;
+    const long = co[1] as number;
+    const lat = co[2] as number;
+    if (countriesProp.includes(name)) {
+      markerList.push({ markerOffset: 15, name, coordinates: [lat, long] });
+    }
+  });
+  return markerList;
+};
 const Map = ({ countryList }: Props) => {
   const [markers2, setMarkers] = React.useState<Marker[]>([]);
   const [textSize, setTextSize] = React.useState(0);
+
+  const [scale, setScale] = React.useState(100);
+  const [center, setCenter] = React.useState<[number, number]>([0, 0]);
   React.useEffect(() => {
-    countries.forEach((co) => {
-      const name = co[3] as string;
-      const long = co[1] as number;
-      const lat = co[2] as number;
-      if (countryList.includes(name)) {
-        setMarkers((olds) => [
-          ...olds,
-          { markerOffset: 15, name, coordinates: [lat, long] },
-        ]);
-      }
-    });
+    const list = generateMarks(countryList);
+    setMarkers(list);
   }, [countryList]);
+  // some kind of loop that loops over the countries, zooms in on them , shows amount of pizzas in there, and move onto next one
+
+  React.useEffect(() => {
+    const length = markers2.length;
+    let ind = 0;
+    const countryRotation = setInterval(() => {
+      if (length > 1) {
+        if (ind < length) {
+          setCenter(markers2[ind].coordinates);
+          setScale(500);
+          ind++;
+        } else {
+          setScale(100);
+          setCenter([0, 0]);
+          ind = 0;
+        }
+      }
+    }, 3000);
+    return () => clearInterval(countryRotation);
+  }, [markers2]);
   return (
-    <ComposableMap
-      projection='geoMercator'
-      style={{ width: '40rem', strokeWidth: '1' }}
-      fill='#f7f7f7'
-      stroke='#a8a8a8'
-      strokeWidth={1}>
-      <ZoomableGroup
-        onMoveEnd={({ zoom }) => {
-          zoom > 4 ? setTextSize(Math.floor(16 - zoom)) : setTextSize(0);
-        }}>
-        <Geographies geography={geoUrl}>
-          {({ geographies }) =>
-            geographies.map((geo) => {
-              const countryNameMatch = markers2.find(
-                (marker: Marker) =>
-                  marker.name.toLowerCase() ===
-                  geo.properties.name.toLowerCase()
-              );
-              return (
-                <Geography
-                  key={geo.rsmKey}
-                  geography={geo}
-                  fill={countryNameMatch ? '#b7e1ba' : '#f7f7f7'}
-                />
-              );
-            })
-          }
-        </Geographies>
-        {markers2.map(({ index, name, coordinates, markerOffset }) => (
-          <Marker key={index} coordinates={coordinates}>
-            <g
-              fill='none'
-              stroke='#FF5533'
-              strokeWidth='1'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              transform='translate(-12, -24)'>
-              <circle
-                cx='12'
-                cy='10'
-                r='3'
-                fill='#FF5533'
+    <Card style={{ position: 'relative' }}>
+      <Backdrop open={markers2.length < 1} style={{ position: 'absolute' }}>
+        <Loading />
+      </Backdrop>
+      <ComposableMap
+        projection='geoMercator'
+        style={{
+          width: '100%',
+          strokeWidth: '1',
+        }}
+        fill='#f7f7f7'
+        stroke='#a8a8a8'
+        projectionConfig={{ scale, center }}
+        strokeWidth={1}>
+        <ZoomableGroup
+          onMoveEnd={({ zoom }) => {
+            zoom > 4 ? setTextSize(Math.floor(16 - zoom)) : setTextSize(0);
+          }}>
+          <Geographies geography={worldCountriesJSON}>
+            {({ geographies }) =>
+              geographies.map((geo) => {
+                const countryNameMatch = markers2.find(
+                  (marker: Marker) =>
+                    marker.name.toLowerCase() ===
+                    geo.properties.name.toLowerCase()
+                );
+                return (
+                  <Geography
+                    key={geo.rsmKey}
+                    geography={geo}
+                    fill={countryNameMatch ? '#b7e1ba' : '#f7f7f7'}
+                  />
+                );
+              })
+            }
+          </Geographies>
+          {markers2.map(({ index, name, coordinates, markerOffset }) => (
+            <Marker key={index} coordinates={coordinates}>
+              <g
+                fill='none'
                 stroke='#FF5533'
                 strokeWidth='1'
-              />
-              <path d='M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z' />
-            </g>
-            <text
-              textAnchor='middle'
-              y={textSize}
-              fill='black'
-              strokeWidth={0}
-              style={{ fontSize: `${textSize}px`, fontFamily: 'Arial' }}>
-              {name}
-            </text>
-          </Marker>
-        ))}
-      </ZoomableGroup>
-    </ComposableMap>
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                transform='translate(-12, -24)'>
+                <circle
+                  cx='12'
+                  cy='10'
+                  r='3'
+                  fill='#FF5533'
+                  stroke='#FF5533'
+                  strokeWidth='1'
+                />
+                <path d='M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z' />
+              </g>
+              <text
+                textAnchor='middle'
+                y={textSize}
+                fill='black'
+                strokeWidth={0}
+                style={{ fontSize: `${textSize}px`, fontFamily: 'Arial' }}>
+                {name}
+              </text>
+            </Marker>
+          ))}
+        </ZoomableGroup>
+      </ComposableMap>
+    </Card>
   );
 };
 
